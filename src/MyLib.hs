e1a3f8a24e96ebf3134805c5db7bcdf245fe5950
fvrrrn <boris.chernystrand@gmail.com>
2024-03-18 21:39:02 +0300

    chapter 5: add qualified imports

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,14 +1,14 @@
 module MyLib (countNonPrintableCharacters, countNonPrintableCharactersInText) where
-import Data.Char
+import qualified Data.Char
 import Data.Text
 
 
 countNonPrintableCharacters :: String -> Int
 countNonPrintableCharacters =
-  Prelude.length . Prelude.filter (not . isPrint)
+  Prelude.length . Prelude.filter (not . Data.Char.isPrint)
 
 -- pack :: String -> Text
 countNonPrintableCharactersInText :: String -> Int
 countNonPrintableCharactersInText =
-  Data.Text.length . Data.Text.filter (not . isPrint) . pack
+  Data.Text.length . Data.Text.filter (not . Data.Char.isPrint) . pack
 
