822c586b75f1431ae84b335a15d1022c2cf8b258
fvrrrn <boris.chernystrand@gmail.com>
2024-03-27 13:42:08 +0300

    chapter 5: add making code available for re-use

--- src/ModuleExamples/Module.hs
+++ src/ModuleExamples/Module.hs
@@ -1,4 +1,46 @@
-module ExampleModules.Module where
+{-# LANGUAGE RecordWildCards #-}
 
-f::IO()
-f = putStrLn "Hello"
+module ModuleExamples.Module where
+
+data Name = Name {getName :: String}
+
+data Salutation = Salutation {getSalutation :: String}
+
+data GreetingMessage = GreetingMessage
+  { greetingSalutation :: Salutation,
+    greetingTo :: Name,
+    greetingFrom :: [Name]
+  }
+
+defaultMessage :: GreetingMessage
+defaultMessage =
+  GreetingMessage
+    { greetingSalutation = Salutation "Hello",
+      greetingTo = Name "Friend",
+      greetingFrom = []
+    }
+
+formatMessage :: GreetingMessage -> String
+formatMessage GreetingMessage {..} =
+  greetingWithSuffix
+  where
+    basicGreeting =
+      getSalutation greetingSalutation <> " " <> getName greetingTo
+    greetingWithSuffix =
+      case greetingFrom of
+        [] ->
+          basicGreeting <> "!"
+        [friend] ->
+          basicGreeting <> ", from: " <> getName friend
+        [friendA, friendB] ->
+          basicGreeting <> ", from: " <> getName friendA <> " and " <> getName friendB
+        friends -> basicGreeting <> ", from your friends: " <> formatFriendList friends
+    formatFriendList friends =
+      case friends of
+        [] -> ""
+        [friend] -> "and " <> getName friend
+        (friend : moreFriends) -> getName friend <> ", " <> formatFriendList moreFriends
+
+excitingMessage :: String -> String
+excitingMessage message =
+  "Exciting news: " <> message <> "!!!"
