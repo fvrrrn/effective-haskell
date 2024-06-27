1ef8f6b57e10a83281c3f63fa8210aa512d0bb66
fvrrrn <boris.chernystrand@gmail.com>
2024-06-27 22:02:11 +0300

    update chapter 7

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,30 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use >=>" #-}
+{-# HLINT ignore "Use >>" #-}
 module Main where
-import qualified MyLib (countNonPrintableCharacters)
 
+returnHello :: IO (IO String)
+returnHello = return (return "Hello")
+
+returnIOWA :: IO (IO wa) -> IO wa
+returnIOWA io = io >>= id
+
+returnArrayIO :: [IO wa] -> IO [wa]
+returnArrayIO [] = return []
+returnArrayIO (x:xs) = do
+  x' <- x
+  xs' <- returnArrayIO xs
+  return $ x' : xs'
+
+-- main :: IO ()
+-- main =
+--   returnHello >>= \io -> io >>= putStrLn
+-- main :: IO ()
+-- main =
+--   returnIOWA (return (return "Hello from IOWA")) >>= putStrLn
 
 main :: IO ()
-main = do
-  print (MyLib.countNonPrintableCharacters "\v\t\aHello\r\n")
+main =
+  returnIOWA (return (return "Hello from IOWA")) >>= putStrLn
+
+
